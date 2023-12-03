7ec427075bba41c50617090faf0bece0d1d0e5c4
Evan Fleming <evan.gordon.fleming@gmail.com>
2023-12-02 17:29:41 -0800

    day 1 complete

---
+++ day-1.exs
@@ -0,0 +1,97 @@
+defmodule Day1 do
+  def process() do
+    File.stream!("inputs/day-1", [:read, :utf8], :line)
+    |> Enum.reduce(0, fn line, acc ->
+      result = Day1.process_line(line)
+      # IO.inspect(result)
+      case  result do
+        {nil, nil} -> acc
+        {first, nil} -> acc + (first * 10) + first
+        {first, last} -> acc + (first * 10) + last
+      end
+    end)
+    |> IO.puts()
+  end
+
+  def process_line(line) do
+    Regex.scan(~r/(?:[1-9]|nineight|oneight|twone|sevenine|fiveight|threeight|eighthree|eightwo|one|two|three|four|five|six|seven|eight|nine)/i, line)
+    |> Enum.reduce({nil, nil}, fn [substr], {first, last} ->
+      case String.to_charlist(substr) do
+        [char] ->
+          case {first, last} do
+            {nil, nil} -> {char - 48, nil}
+            {_, _} -> {first, char - 48}
+          end
+        word ->
+          IO.puts(word)
+          values = convert_word(word)
+          case values do
+            [x] ->
+              case {first, last} do
+                {nil, nil} -> {x, nil}
+                {_, _} -> {first, x}
+              end
+            [x, y] ->
+              IO.inspect({x, y})
+              case {first, last} do
+                {nil, nil} -> {x, y}
+                {_, _} -> {first, y}
+              end
+          end
+
+      end
+    end)
+  end
+
+  def convert_word(word) do
+    case word do
+      ~c"nineight" -> [9, 8]
+      ~c"oneight" -> [1, 8]
+      ~c"twone" -> [2, 1]
+      ~c"sevenine" -> [7, 9]
+      ~c"fiveight" -> [5, 8]
+      ~c"threeight" -> [3, 8]
+      ~c"eighthree" -> [8, 3]
+      ~c"eightwo" -> [8, 2]
+      ~c"one" -> [1]
+      ~c"two" -> [2]
+      ~c"three" -> [3]
+      ~c"four" -> [4]
+      ~c"five" -> [5]
+      ~c"six" -> [6]
+      ~c"seven" -> [7]
+      ~c"eight" -> [8]
+      ~c"nine" -> [9]
+    end
+  end
+end
+
+# pattern = ~r/(?:[1-9]|[one-two-three-four-five-six-seven-eight-nine]+)/
+# matches = Regex.scan(pattern, input_string)
+
+# defmodule Day1 do
+#   def process_line(line) do
+#     String.to_charlist(line)
+#     |> Enum.reduce({nil, nil}, fn char, {first, last} ->
+#       case {first, last} do
+#         {nil, nil} when char >= 48 and char <= 57 -> {char - 48, nil}
+#         {_, _} when char >= 48 and char <= 57 -> {first, char - 48}
+#         {_, _} -> {first, last}
+#       end
+#     end)
+#   end
+
+#   def process() do
+#     File.stream!("inputs/day-1", [:read, :utf8], :line)
+#     |> Enum.reduce(0, fn line, acc ->
+#       case  Day1.process_line(line) do
+#         {nil, nil} -> acc
+#         {first, nil} -> acc + (first * 10) + first
+#         {first, last} -> acc + (first * 10) + last
+#       end
+#     end)
+#     |> IO.puts()
+#   end
+# end
+
+Day1.process()
